cc72d5948138ba2ed98220842db9a50323e4b5dc
Gordon Tyler <gordon@doxxx.net>
2015-06-28 17:53:53 -0400

    Refactor into single-threaded design for simplicity.

--- main.py
+++ main.py
@@ -1,37 +1,96 @@
-import recipe_list
 import json
-from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urljoin
 
-def fetch(params):
-    cls, level_range = params
-    print("Fetching {0}:{1}...".format(cls, level_range))
-    links = recipe_list.fetch_list("en", cls, level_range)
-    return {
-        "class": cls,
-        "recipes": [recipe_list.fetch_recipe(link) for link in links]
+import requests_cache
+import lxml.html as html
+
+
+BASE_URL = {
+    "en": "http://na.finalfantasyxiv.com/lodestone/playguide/db/recipe/",
+    "ja": "http://jp.finalfantasyxiv.com/lodestone/playguide/db/recipe/",
+    "fr": "http://fr.finalfantasyxiv.com/lodestone/playguide/db/recipe/",
+    "de": "http://de.finalfantasyxiv.com/lodestone/playguide/db/recipe/",
+}
+
+CLASSES = [
+    "Carpenter",
+    "Blacksmith",
+    "Armorer",
+    "Goldsmith",
+    "Leatherworker",
+    "Weaver",
+    "Alchemist",
+    "Culinarian",
+]
+
+LEVEL_RANGE = ["{0}-{1}".format(start, start + 4) for start in range(1, 60, 5)]
+NUM_LEVEL_RANGES = len(LEVEL_RANGE)
+
+session = requests_cache.CachedSession()
+
+
+def parse_recipe_links_page(base_url, r):
+    tree = html.fromstring(r.text)
+    rel_links = tree.xpath("//div/@data-ldst-href")
+    links = [urljoin(base_url, rel_link) for rel_link in rel_links]
+    show_end = int(tree.xpath("//span[@class='show_end']/text()")[0])
+    total = int(tree.xpath("//span[@class='total']/text()")[0])
+    return links, show_end, total
+
+
+def fetch_recipe_links_page(cls, level_range, page):
+    base_url = BASE_URL["en"]
+
+    params = {
+        "category2": CLASSES.index(cls),
+        "category3": level_range,
+        "page": page,
     }
 
-def add_recipe_metadata(recipe, metadata):
-    r = recipe.copy()
-    r.update(metadata)
-    return r
+    print("Fetching {0} {1} page {2}".format(cls, LEVEL_RANGE[level_range], page))
+
+    return parse_recipe_links_page(base_url, session.get(base_url, params=params))
+
+def fetch_recipe_links(cls):
+    links = []
+
+    for level_range in range(0, NUM_LEVEL_RANGES):
+        page = 1
+        while True:
+            page_links, show_end, total = fetch_recipe_links_page(cls, level_range, page)
+            links += page_links
+            if show_end < total:
+                page += 1
+            else:
+                break
+
+    return links
+
+def fetch_recipe(url):
+    print("Fetching recipe: {0}".format(url))
+    r = session.get(url)
+    tree = html.fromstring(r.text)
+    return {
+        "name" : tree.xpath("//h2[contains(@class,'item_name')]/text()")[0],
+        "level" : int(tree.xpath("//div[@class='recipe_level']/span/text()")[0]),
+        "difficulty" : int(tree.xpath("//dl/dt[text()='Difficulty']/following-sibling::dd[1]/text()")[0]),
+        "durability" : int(tree.xpath("//dl/dt[text()='Durability']/following-sibling::dd[1]/text()")[0]),
+        "maxQuality" : int(tree.xpath("//dl/dt[text()='Maximum Quality']/following-sibling::dd[1]/text()")[0]),
+    }
+
+def fetch(cls):
+    return [fetch_recipe(link) for link in fetch_recipe_links(cls)]
+
+def scrape_to_file(cls):
+    recipes = fetch(cls)
+    with open("out/" + cls + ".json", "wt") as db_file:
+        json.dump(recipes, db_file, indent="  ")
+    print("Wrote {0} recipes for {1}".format(len(recipes), cls))
 
 def main():
-    db_files = {cls:open("out/" + cls + ".json", "wt") for cls in recipe_list.CLASSES}
+    for cls in CLASSES:
+        scrape_to_file(cls)
 
-    with ThreadPoolExecutor(max_workers=4) as ex:
-        for cls in recipe_list.CLASSES:
-            with db_files[cls] as db_file:
-                #metadata = { "cls": cls }
-                combined_results = []
-                params = [(cls, level_range) for level_range in range(0, 12)]
-                for results in ex.map(fetch, params):
-                    cls = results["class"]
-                    recipes = results["recipes"]
-                    print("Adding {0} results for {1}".format(len(recipes), cls))
-                    #recipes = [add_recipe_metadata(r, metadata) for r in recipes]
-                    combined_results += recipes
-                json.dump(combined_results, db_file, indent="  ")
 
 if __name__ == '__main__':
     main()
